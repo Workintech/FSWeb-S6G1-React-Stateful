86769b15f1a22c69143d053ecad6f9895919f407
samilkafa <samilkafa@gmail.com>
2023-03-03 19:42:14 +0300

    Hafta-6-Proje-1

--- src/components/Sayac.js
+++ src/components/Sayac.js
@@ -46,18 +46,23 @@ ADIM 6:
 */
 
 import React from 'react'; /* ADIM 0 buraya*/
+import { useState } from 'react';
 
 export default function Sayac() {
   /* ADIM 1 buraya*/
+  const [sayici, setSayici] = useState(0);
 	
 	
   const artirici = () => {
     /* ADIM 4 buraya */
+    setSayici(sayici + 1);
   };
   const azaltici = () => {
     /* ADIM 5 */
+    setSayici(sayici - 1);
   };
   const reset = () => {
+    setSayici(0);
   };
 
   const stil = {
@@ -70,7 +75,7 @@ export default function Sayac() {
     <div className='widget-counter container'>
       <h2>Sayaç</h2>
       <div id='sayici' style={stil}>
-        Sayı {sayici} {/* ADIM 3  buraya*/ }
+        Sayı {sayici} { sayici % 2 == 0 ? "çift" : "tek" }
       </div>
       <div>
         <button id='artirici' onClick={artirici}>Artırıcı</button>
